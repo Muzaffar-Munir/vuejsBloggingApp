c8723e975dfb0fb80a46c98fa3d9a82185952b45
Muhammad Afzaal <muhammad.afzaal@cireson.com>
2023-10-19 18:00:12 +0500

    Blog create form

---
+++ src/components/dashboard/create.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <!-- Content Wrapper. Contains page content -->
+
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard v3</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><router-link to="/dashboard/home">Home</router-link></li>
+                            <li class="breadcrumb-item active">Dashboard v3</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-default">
+                            <div class="card-header">
+                                <h3 class="card-title">Create Belog</h3>
+                            </div>
+                            <div class="card-body p-2">
+                                <div class="bs-stepper">
+
+                                    <div class="bs-stepper-content">
+                                        <!-- your steps content here -->
+                                        <div id="logins-part" class="content" role="tabpanel"
+                                            aria-labelledby="logins-part-trigger">
+                                            <div class="form-group">
+                                                <label for="title">Title</label>
+                                                <input type="text" class="form-control" id="title" placeholder="Enter title"
+                                                    v-model="title">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="content">content</label>
+                                                <textarea type="text" class="form-control" id="content"
+                                                    placeholder="Enter content" v-model="content"> </textarea>
+                                            </div>
+                                            <button class="btn btn-primary" @click="createBelog">Create</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'BelogCreate',
+    data() {
+        return {
+            title: '',
+            content: ''
+        }
+    },
+    methods: {
+        createBelog() {
+            console.log("AAAAAAAAa ", this.title, this.content);
+        }
+    }
+}
+</script>
+
+<style>
+.login-box-center {
+    margin: 5rem auto;
+}
+</style>
+  
+  
+  
